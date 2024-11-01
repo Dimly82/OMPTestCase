296644884cbcce80e78ee69c08405fdca34f9536
Dimly82 <pdimochka@gmail.com>
2024-11-01 17:46:54 +0300

    feat: checking for permission when getting system time

--- com/system/time/time_service.h
+++ com/system/time/time_service.h
@@ -6,19 +6,50 @@
 #include "time_adaptor.h"
 
 class Time : public sdbus::AdaptorInterfaces<com::system::time_adaptor> {
- public:
+public:
   Time(sdbus::IConnection &connection, const sdbus::ObjectPath &objectPath)
-      : AdaptorInterfaces(connection, objectPath) {
+          : AdaptorInterfaces(connection, objectPath) {
     registerAdaptor();
   }
 
   ~Time() { unregisterAdaptor(); }
 
- protected:
+protected:
   uint64_t GetSystemTime() override {
+    // Get name of the executable
+    std::string exePath =
+            "/proc/" +
+            std::to_string(
+                    getObject().getCurrentlyProcessedMessage()->getCredsPid()) +
+            "/exe";
+    char resolvedPath[PATH_MAX];
+    ssize_t len =
+            readlink(exePath.c_str(), resolvedPath, sizeof(resolvedPath) - 1);
+
+    if (len == -1) {
+      throw sdbus::Error(
+              "com.system.time.Error",
+              "Failed to read executable path for PID " +
+              std::to_string(
+                      getObject().getCurrentlyProcessedMessage()->getCredsPid()));
+    }
+    resolvedPath[len] = '\0';
+
+//     Connecting to com.system.permissions service and calling CheckApplicationHasPermission method
+    auto proxy = sdbus::createProxy("com.system.permissions", "/");
+    proxy->finishRegistration();
+    bool hasPermission = false;
+    proxy->callMethod("CheckApplicationHasPermission").onInterface("com.system.permissions").
+            withArguments(resolvedPath, 0).storeResultsTo(hasPermission);
+
+    if (!hasPermission)
+      throw sdbus::Error("com.system.time.UnauthorizedAccess",
+                         "Application does not have permission to use this method");
+
+    // Getting current system time
     auto now = std::chrono::system_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(
-        now.time_since_epoch());
+            now.time_since_epoch());
     return duration.count();
   }
 };
